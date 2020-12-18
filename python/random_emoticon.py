fb6871206ba5addc8b617a52fadd920b2efca042
Marc Santos <sntosbiz@gmail.com>
2020-12-17 19:48:12 -0800

    Updated Lab 05

--- python/random_emoticon.py
+++ python/random_emoticon.py
@@ -7,11 +7,17 @@ eye = [':', '8', '=', ';']
 nose = ['', '^', '-']
 mouth = [')', ']', '(', '[']
 
+v_1 = ['', '(', '[']
+v_2 = ['^', '-', '*']
+v_3 = ['_', '.']
+v_4 = ['', ')', ']']
+
 while True:
     print(random.choice(eye) + random.choice(nose) + random.choice(mouth)) 
+    print(random.choice(v_1) + random.choice(v_2) + random.choice(v_3) + random.choice(v_2) + random.choice(v_4)) 
     print(random.choice(eye) + random.choice(nose) + random.choice(mouth)) 
+    print(random.choice(v_1) + random.choice(v_2) + random.choice(v_3) + random.choice(v_2) + random.choice(v_4)) 
     print(random.choice(eye) + random.choice(nose) + random.choice(mouth)) 
-    print(random.choice(eye) + random.choice(nose) + random.choice(mouth)) 
-    print(random.choice(eye) + random.choice(nose) + random.choice(mouth)) 
+    
     break
-
+    
