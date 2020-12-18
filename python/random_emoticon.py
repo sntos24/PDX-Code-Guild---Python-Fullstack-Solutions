dce7949129a0fb367722e825a809c95a23ad154b
Marc Santos <sntosbiz@gmail.com>
2020-12-17 19:31:36 -0800

    Added Random Emoticon

---
+++ python/random_emoticon.py
@@ -0,0 +1,17 @@
+"""
+Random Emoticon Lab by Marc Santos
+"""
+import random
+
+eye = [':', '8', '=', ';']
+nose = ['', '^', '-']
+mouth = [')', ']', '(', '[']
+
+while True:
+    print(random.choice(eye) + random.choice(nose) + random.choice(mouth)) 
+    print(random.choice(eye) + random.choice(nose) + random.choice(mouth)) 
+    print(random.choice(eye) + random.choice(nose) + random.choice(mouth)) 
+    print(random.choice(eye) + random.choice(nose) + random.choice(mouth)) 
+    print(random.choice(eye) + random.choice(nose) + random.choice(mouth)) 
+    break
+
